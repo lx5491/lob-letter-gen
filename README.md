df7df53ed841a2a9812536797d9222495bcf59e8
lx5491 <lx5491@gmail.com>
2016-09-29 23:56:55 -0400

    removed all the debug printing code; added requirements.txt

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # lob-letter-gen
 This is the coding challenge for Lob
+
+## Pre-requisites: all the necessary python packages are in requirements.txt
+
+## Instructions for running
+1. pip install -r requirements.txt
+2. The usage of letter.py:
+    python letter.py [-h] -name NAME -addr1 ADDR1 [-addr2 ADDR2] -city CITY -state
+                 STATE -zip ZIP -message MESSAGE [-html HTML] [-lobkey LOBKEY]
+                 [-googlekey GOOGLEKEY]
+    2a. 
--- letter.py
+++ letter.py
@@ -2,11 +2,17 @@ import sys, os
 import argparse
 
 import lob
+from lob.error import InvalidRequestError
 from apiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-
 class LetterGenerator(object):
+    # static class error constants
+
+    OK = 0
+    LEGISLATOR_NO_ADDRESS = 400
+    LEGISLATOR_NOT_FOUND = 404
+
     def __init__(self, args_dict):
         self.from_name = args_dict["name"]
         self.from_addr1 = args_dict["addr1"]
@@ -20,13 +26,17 @@ class LetterGenerator(object):
         with open(args_dict["html"]) as html_file:
             self.html_string = html_file.read()
 
-        self.lob_api_key = "test_a1418fba3569832462d2c08d24cf03248b8"
+        self.lob_api_key = args_dict["lobkey"]
         lob.api_key = self.lob_api_key
 
-        self.google_api_key = "AIzaSyBqrexHwjVA2hgS81GyqjU8vzRMiesc8vU"
+        self.google_api_key = args_dict["googlekey"]
         self.civic_service = build('civicinfo', 'v2', developerKey=self.google_api_key)
 
     def generate_letter(self):
+        from_address = self.create_address(name=self.from_name, address_line1=self.from_addr1, \
+                            address_line2=self.from_addr2, address_city=self.from_city, \
+                            address_state=self.from_state, address_zip=self.from_zip,
+                            address_country="US")
         official = self.access_legislator_info()
         if(official is not None):
             self.to_name = official["name"]
@@ -37,18 +47,17 @@ class LetterGenerator(object):
             self.to_state = official["address"][0]["state"]
             self.to_zip = official["address"][0]["zip"]
 
-            from_address = self.create_address(name=self.from_name, address_line1=self.from_addr1, \
-                                address_line2=self.from_addr2, address_city=self.from_city, \
-                                address_state=self.from_state, address_zip=self.from_zip,
-                                address_country="US")
             to_address = self.create_address(name=self.to_name, address_line1=self.to_addr1, \
                                 address_line2=self.to_addr2, address_city=self.to_city, \
                                 address_state=self.to_state, address_zip=self.to_zip,
                                 address_country="US")
 
             letter = self.create_letter(from_address, to_address)
-            print letter
-            print "PDF URL:", letter["url"]
+            return letter["url"]
+        elif(len(official["address"])):
+            return LetterGenerator.LEGISLATOR_NO_ADDRESS
+        else:
+            return LetterGenerator.LEGISLATOR_NOT_FOUND
 
     def access_legislator_info(self):
         full_from_address = ""
@@ -57,14 +66,12 @@ class LetterGenerator(object):
         full_from_address += ", " + self.from_city
         full_from_address += ", " + self.from_state
         full_from_address += " " + self.from_zip
-        print "full_from_address:", full_from_address
+        # print "full_from_address:", full_from_address
         request = self.civic_service.representatives().representativeInfoByAddress(
                 levels=["country"], roles=["legislatorLowerBody", "legislatorUpperBody"], \
                 address=full_from_address, includeOffices=None)
         try:
             response = request.execute()
-            print "response:", type(response)
-            print response
             if(len(response['officials']) > 0):
                 official = response['officials'][0]
             else:
@@ -73,8 +80,8 @@ class LetterGenerator(object):
             return official
 
         except HttpError as e:
-            print "Error,", e.resp.status
-            print "Reason:", e._get_reason().strip()
+            # print "Error,", e.resp.status
+            # print "Reason:", e._get_reason().strip()
             return None
 
     def create_address(self, name, address_line1, address_line2, address_city, address_state, address_zip, address_country="US", description=None):
@@ -110,25 +117,31 @@ class LetterGenerator(object):
         return letter
 
 
-
 if __name__ == "__main__":
     program_name = sys.argv[0]
 
     parser = argparse.ArgumentParser(description='Generate a letter PDF using Lob API')
-    parser.add_argument("-name", required=True)
-    parser.add_argument("-addr1", required=True)
-    parser.add_argument("-addr2")
-    parser.add_argument("-city", required=True)
-    parser.add_argument("-state", required=True)
-    parser.add_argument("-zip", required=True)
-    parser.add_argument("-message", required=True)
-    parser.add_argument("-html", default="index.html")
+    parser.add_argument("-name", required=True, help='Your name')
+    parser.add_argument("-addr1", required=True, help='Your address line 1')
+    parser.add_argument("-addr2", help='Your address line 2')
+    parser.add_argument("-city", required=True, help='Your city')
+    parser.add_argument("-state", required=True, help='Your state')
+    parser.add_argument("-zip", required=True, help='Your address zip code')
+    parser.add_argument("-message", required=True, help='Your message to legislator')
+    parser.add_argument("-html", default="index.html", help='The html template for the letter, default to be index.html')
+    parser.add_argument("-lobkey", default="test_a1418fba3569832462d2c08d24cf03248b8", help='Lob API key, default to be Simon\'s key')
+    parser.add_argument("-googlekey", default="AIzaSyBqrexHwjVA2hgS81GyqjU8vzRMiesc8vU", help='Google Civic API key, default to be Simon\'s key')
 
     args_dict = vars(parser.parse_args())
 
     letter_gen = LetterGenerator(args_dict)
-    letter_gen.generate_letter()
-
+    result = letter_gen.generate_letter()
+    if(type(result) is unicode or type(result) is str):
+        print "The PDF file of the letter is at:", result
+    elif(result == LetterGenerator.LEGISLATOR_NO_ADDRESS):
+        print "The legislator has no address"
+    elif(result == LetterGenerator.LEGISLATOR_NOT_FOUND):
+        print "No legislator is found given the information"
 
 
 
